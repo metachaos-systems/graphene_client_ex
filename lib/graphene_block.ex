054d06303be5bc4a0bdf8686c3e29c5d02515e1a
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-02 20:36:53 +0300

    add unpack_tx placeholder

--- lib/graphene_block.ex
+++ lib/graphene_block.ex
@@ -1,3 +1,8 @@
 defmodule Graphene.Block do
   defstruct [:height, :transactions, :previous, :transactions_merkle_root, :witness, :witness_signature, :timestamp, :extensions]
+
+  def unpack_txs(block) do
+    for tx <- block.transactions, do: tx
+  end
+
 end
