116c02088feca2bc1c5cfa50fcc41277020063fa
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-04 15:31:10 +0300

    use keyword lists for args

--- lib/stages/stage_supervisor.ex
+++ lib/stages/stage_supervisor.ex
@@ -8,7 +8,8 @@ defmodule Graphene.Stage.Supervisor do
 
   def init(:ok) do
     children = [
-      worker(Stage.Blocks.Producer, [[], [name: Stage.Blocks.Producer]]),
+      worker(Graphene.Stage.Blocks.Producer, [[], [name: Stage.Blocks.Producer]]),
+      worker(Graphene.Stage.Blocks.ExampleConsumer, [[subscribe_to: [Stage.Blocks.Producer]]])
     ]
     supervise(children, strategy: :one_for_all)
   end
